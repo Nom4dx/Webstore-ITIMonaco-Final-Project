78308874ad6847d5ae5aab0c6cd9c49d5435b0c6
Diell <tua@email.com>
2026-04-01 13:11:11 +0200

    Add methods for user deletion and modification in DataBase class

--- php/database.php
+++ php/database.php
@@ -14,6 +14,14 @@ class DataBase {
         return $this->conn->query($sql);
     }
 
+    public function eliminazione($del_user){
+        $sql_del = "DELETE FROM utenti WHERE utente='$del_user'";
+        $this->query($sql_del);
+    }
+    public function modifica($utente, $password, $email, $nome, $ruolo){
+        $sql_mod = "UPDATE utenti SET password='$password', email='$email', nome='$nome', ruolo='$ruolo' WHERE utente='$utente'";
+        $this->query($sql_mod);
+    }
 
 }
 ?>
